c9d29814e929bef00bc2038cdfc9dd147ae4981c
Fabrizio <zambranafabrizio@gmail.com>
2020-10-11 18:26:56 -0400

    Practica4 hospital DIA

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+            Paciente jose = new Paciente("Jose","Perez","García",4455);
+            System.out.println(jose);
+            Medico felipe = new Medico("Felipe","Cardenas"," ",4489755);
+            System.out.println(felipe);
+    }
+}
